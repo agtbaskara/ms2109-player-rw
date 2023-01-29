8edc891162495e98923fba75187a908814db99ff
jackson-57 <49173011+jackson-57@users.noreply.github.com>
2023-01-28 16:22:10 -0800

    Reformat code

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Web Player for MacroSilicon MS2109 Capture Cards
 
-I recently bought a cheap (65CNY/~10USD) capture card with MS2109 chip to play Switch games on my monitor without an HDMI switch (no pun intended).
+I recently bought a cheap (65CNY/~10USD) capture card with MS2109 chip to play Switch games on my monitor without an
+HDMI switch (no pun intended).
 
 ## Hardware ID
 
-|  VID  |  PID  |  REV  |
-| :---: | :---: | :---: |
-| 534d  | 2109  | 2100  |
+| VID  | PID  | REV  |
+|:----:|:----:|:----:|
+| 534d | 2109 | 2100 |
 
 ## Spec
 
 | Key                  | Value                   |
-| -------------------- | ----------------------- |
+|----------------------|-------------------------|
 | Video Input          | 4K 30FPS / 1080P 60FPS  |
 | Video Output (MJPEG) | 1080P 30FPS             |
 | Video Output (YUY2)  | 1080P 5FPS / 720P 10FPS |
@@ -20,18 +21,24 @@ I recently bought a cheap (65CNY/~10USD) capture card with MS2109 chip to play S
 Notes:
 
 1. Some device claims 1080P 60FPS output support but that's fake. When selected, they still output at 30FPS.
-2. When connected to a USB hub, 1080P 30FPS mode may display a black screen (maybe due to insufficient bandwidth). Lower resolution or frame rate (for example 1080P 25FPS) may be used instead.
-3. The audio output is actually stereo 16bit 48kHz interleaved (1 sample for left channel, then 1 sample for right channel, alternating).
+2. When connected to a USB hub, 1080P 30FPS mode may display a black screen (maybe due to insufficient bandwidth). Lower
+   resolution or frame rate (for example 1080P 25FPS) may be used instead.
+3. The audio output is actually stereo 16bit 48kHz interleaved (1 sample for left channel, then 1 sample for right
+   channel, alternating).
 
 ## Use with OBS
 
-1. Every time you remove and re-plug the device, you must re-select the "Device" in each source (or re-create all sources).
-2. If 1080P 30FPS MJPEG doesn't work (black screen), try 25FPS or another USB port. Set "Resolution" to "Highest FPS" won't work either.
+1. Every time you remove and re-plug the device, you must re-select the "Device" in each source (or re-create all
+   sources).
+2. If 1080P 30FPS MJPEG doesn't work (black screen), try 25FPS or another USB port. Set "Resolution" to "Highest FPS"
+   won't work either.
 3. "Audio Output Mode" option in "Video Capture Device" can be very laggy, to reduce lag:
-   1. Add an "Audio Input Capture" source
-   2. Set "Device" to "Digital Audio Interface (*X*- USB Digital Audio)" (where *X* may be any number)
-   3. Open "Edit" -> "Advanced Audio Properties" menu, set "Audio Monitoring" to "Monitor Only" or "Monitor and Output" (depends on your usage)
-4. If you want to use YUY2 (high quality, low FPS) but can't find it in "Video Format", first set "FPS" to "Highest FPS" or 720P 10FPS, now you should see it.
+    1. Add an "Audio Input Capture" source
+    2. Set "Device" to "Digital Audio Interface (*X*- USB Digital Audio)" (where *X* may be any number)
+    3. Open "Edit" -> "Advanced Audio Properties" menu, set "Audio Monitoring" to "Monitor Only" or "Monitor and
+       Output" (depends on your usage)
+4. If you want to use YUY2 (high quality, low FPS) but can't find it in "Video Format", first set "FPS" to "Highest FPS"
+   or 720P 10FPS, now you should see it.
 5. For stereo audio: (source: https://www.youtube.com/watch?v=R4SXJMNywL4&lc=Ugwu3DawGg791wTcold4AaABAg)
     * Linux: Linux kernel has a patch to do this automatically.
     * Windows: https://github.com/ToadKing/mono-to-stereo
@@ -52,4 +59,5 @@ However
 
 * It only support 1080P resolution
 * It only support MJPEG format
-* Only tested on Chrome (Microsoft Edge) on Windows 10. Due to the specific method for finding the device, it may fail on other browsers/OSes
+* Only tested on Chrome (Microsoft Edge) on Windows 10. Due to the specific method for finding the device, it may fail
+  on other browsers/OSes
--- main.js
+++ main.js
@@ -1,77 +1,77 @@
 const MODE_LIST = [
-  // todo: re-evaluate mode list
-  // 720p is clearer than 1080p and allows for 60fps video
-  { width: 1280, height: 720, frameRate: 60 },
-  { width: 1280, height: 720, frameRate: 30 },
+    // todo: re-evaluate mode list
+    // 720p is clearer than 1080p and allows for 60fps video
+    {width: 1280, height: 720, frameRate: 60},
+    {width: 1280, height: 720, frameRate: 30},
 
-  // Fall back to 1080p
-  { width: 1920, height: 1080, frameRate: 30 },
-  // MS2109 may output 25FPS when connected to a USB hub
-  { width: 1920, height: 1080, frameRate: 25 },
+    // Fall back to 1080p
+    {width: 1920, height: 1080, frameRate: 30},
+    // MS2109 may output 25FPS when connected to a USB hub
+    {width: 1920, height: 1080, frameRate: 25},
 ];
 
 const videoElement = document.body.querySelector("video");
 start().then();
 
 async function requestMediaDevicePermission() {
-  // request any media device to trigger the permission popup
-  const stream = await window.navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true,
-  });
+    // request any media device to trigger the permission popup
+    const stream = await window.navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+    });
 
-  // stop all tracks so they can be requested again
-  for (const track of stream.getTracks()) {
-    track.stop();
-  }
+    // stop all tracks so they can be requested again
+    for (const track of stream.getTracks()) {
+        track.stop();
+    }
 }
 
 function findDevice(devices, type, vid, pid) {
-  // Spec doesn't define how to find a device with specified VID/PID
-  // Chrome appends (vid:pid) to the device label
-  // TODO: make sure it works on Firefox/Safari
-  return devices.find(
-    x =>
-      x.kind === type &&
-      x.label.endsWith(`(${vid.toLowerCase()}:${pid.toLowerCase()})`)
-  );
+    // Spec doesn't define how to find a device with specified VID/PID
+    // Chrome appends (vid:pid) to the device label
+    // TODO: make sure it works on Firefox/Safari
+    return devices.find(
+        x =>
+            x.kind === type &&
+            x.label.endsWith(`(${vid.toLowerCase()}:${pid.toLowerCase()})`)
+    );
 }
 
 async function start() {
-      // Only `getUserMedia` triggers the permission popup, `enumerateDevices` won't
-      await requestMediaDevicePermission();
+    // Only `getUserMedia` triggers the permission popup, `enumerateDevices` won't
+    await requestMediaDevicePermission();
 
-      // TODO: handle permission rejected
+    // TODO: handle permission rejected
 
-      const devices = await window.navigator.mediaDevices.enumerateDevices();
-      const videoDevice = findDevice(devices, 'videoinput', '534d', '2109');
-      const audioDevice = findDevice(devices, 'audioinput', '534d', '2109');
+    const devices = await window.navigator.mediaDevices.enumerateDevices();
+    const videoDevice = findDevice(devices, 'videoinput', '534d', '2109');
+    const audioDevice = findDevice(devices, 'audioinput', '534d', '2109');
 
-      // TODO: handle device not found
+    // TODO: handle device not found
 
-      for (const mode of MODE_LIST) {
+    for (const mode of MODE_LIST) {
         try {
-          const videoStream = await window.navigator.mediaDevices.getUserMedia({
-            video: {
-              deviceId: { exact: videoDevice.deviceId },
-              width: { exact: mode.width },
-              height: { exact: mode.height },
-              frameRate: { exact: mode.frameRate },
-            },
-          });
-          videoElement.srcObject = videoStream;
+            const videoStream = await window.navigator.mediaDevices.getUserMedia({
+                video: {
+                    deviceId: {exact: videoDevice.deviceId},
+                    width: {exact: mode.width},
+                    height: {exact: mode.height},
+                    frameRate: {exact: mode.frameRate},
+                },
+            });
+            videoElement.srcObject = videoStream;
 
-          const audioStream = await window.navigator.mediaDevices.getUserMedia({
-            audio: {
-              groupId: { exact: audioDevice.groupId },
-              sampleRate: 96_000,
-              sampleSize: 16,
-            },
-          });
+            const audioStream = await window.navigator.mediaDevices.getUserMedia({
+                audio: {
+                    groupId: {exact: audioDevice.groupId},
+                    sampleRate: 96_000,
+                    sampleSize: 16,
+                },
+            });
 
-          const context = new AudioContext({ sampleRate: 96_000 });
-          const source = context.createMediaStreamSource(audioStream);
-          await context.audioWorklet.addModule('data:application/javascript;charset=utf8,' + encodeURIComponent(`
+            const context = new AudioContext({sampleRate: 96_000});
+            const source = context.createMediaStreamSource(audioStream);
+            await context.audioWorklet.addModule('data:application/javascript;charset=utf8,' + encodeURIComponent(`
             class SplitProcessor extends AudioWorkletProcessor {
               process (inputs, outputs, parameters) {
                 const input = inputs[0][0];
@@ -98,16 +98,16 @@ async function start() {
 
             registerProcessor('split-processor', SplitProcessor)
           `));
-          const processor = new AudioWorkletNode(context, 'split-processor', {
-            numberOfInputs: 1,
-            numberOfOutputs: 1,
-          });
-          source.connect(processor);
-          processor.connect(context.destination);
-          break;
+            const processor = new AudioWorkletNode(context, 'split-processor', {
+                numberOfInputs: 1,
+                numberOfOutputs: 1,
+            });
+            source.connect(processor);
+            processor.connect(context.destination);
+            break;
         } catch (e) {
-          console.error(e);
-          // ignore
+            console.error(e);
+            // ignore
         }
-      }
+    }
 }
